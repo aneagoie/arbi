e8e7be9ea4886dfa99d22c54a257ea73cb5ef8ab
aneagoie <aneagoie1@gmail.com>
2018-03-09 09:54:33 -0500

    cleaning up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css'
 
-const lowestPrice = (array) => {
-  const price = array.map(item => item.last).sort();
-  return price[0];
-}
-
 class App extends Component {
   constructor() {
     super()
@@ -15,19 +10,33 @@ class App extends Component {
         data: null
       }
   }
+
+  lowestPrice = (array) => {
+    const price = array.map(item => item.last).sort();
+    const exchange = array.filter(item => item.last === price[0]);
+    return exchange[0]
+  }
+
   componentDidMount() {
-    let exhangesArray = [];
-    // fetch('https://min-api.cryptocompare.com/data/all/exchanges')
     fetch('https://api.cbix.ca/v1/index')
       .then(response => response.json())
-      .then(exchanges => {
-        const cleanData = exchanges.exchanges.map((exch, i) => {
+      .then(resp => {
+        const exchanges = resp.exchanges;
+        const lowestPriceNumber = this.lowestPrice(exchanges).last;
+        const lowestPriceExchange = this.lowestPrice(exchanges).name;
+        const cleanData = exchanges.map(exch => {
+          if (exch.last/lowestPriceNumber > 1.2) {
+            console.log(`There is arbitrage possibility of ${(exch.last/lowestPriceNumber).toFixed(3)}% between ${exch.name} and ${lowestPriceExchange}.
+                        If you put $10,000 you could make $${parseInt(10000*(exch.last/lowestPriceNumber - 1), 10)}. check out the details: https://aneagoie.github.io/arbi/`);
+          }
           return {
             name: exch.name,
             currency: 'CAD',
-            price: exch.last,
-            volume: exch.volume_24hour,
-            arb: lowestPrice(exchanges.exchanges)/exch.last
+            price: '$ ' + exch.last,
+            ask: '$ ' + exch.ask,
+            bid: '$ ' + exch.bid,
+            volume: exch.volume_24hour + ' BTC',
+            arb: exch.last/lowestPriceNumber
           }
 
         })
@@ -46,12 +55,20 @@ class App extends Component {
       Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
     }, {
       id: 'price',
-      Header: 'Price',
+      Header: 'Last',
       accessor: d => d.price // Custom value accessors!
     }, {
       id: 'arb',
       Header: '%arb',
       accessor: d => d.arb // Custom value accessors!
+    }, {
+      id: 'bid',
+      Header: 'Bid',
+      accessor: d => d.bid // Custom value accessors!
+    }, {
+      id: 'ask',
+      Header: 'Ask',
+      accessor: d => d.ask // Custom value accessors!
     }, {
       id: 'volume',
       Header: 'Volume 24h',
